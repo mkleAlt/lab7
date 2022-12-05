e674e81f641ad49521729af8e2d6431a20db5860
mk <alabamamidomama@gmail.com>
2022-12-05 15:21:52 +0100

    add: point class

---
+++ src/entities/Point.java
@@ -0,0 +1,26 @@
+package entities;
+
+public class Point {
+    private final float x;
+    private final float y;
+    public final static Point START = new Point(0, 0);
+    public final static Point START_FROM_X1 = new Point(1, 0);
+
+    public Point(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + "; " + y + ")";
+    }
+}
