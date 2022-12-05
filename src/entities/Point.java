1424d830dd5aba9ed3b1ca7dee8c1459e42976f2
mk <alabamamidomama@gmail.com>
2022-12-05 15:25:20 +0100

    refactor: make const fields private

--- src/entities/Point.java
+++ src/entities/Point.java
@@ -3,8 +3,8 @@ package entities;
 public class Point {
     private final float x;
     private final float y;
-    public final static Point START = new Point(0, 0);
-    public final static Point START_FROM_X1 = new Point(1, 0);
+    private final static Point START = new Point(0, 0);
+    private final static Point START_FROM_X1 = new Point(1, 0);
 
     public Point(float x, float y) {
         this.x = x;
