2920a4e8f39d08740d1d62718c60ab44a6d187f2
mk <alabamamidomama@gmail.com>
2022-12-05 15:42:37 +0100

    refactor: change constructor to 'of' pattern

--- src/entities/Point.java
+++ src/entities/Point.java
@@ -6,9 +6,13 @@ public class Point {
     private final static Point START = new Point(0, 0);
     private final static Point START_FROM_X1 = new Point(1, 0);
 
-    public Point(double x, double y) {
+    private Point(double x, double y) {
         this.x = x;
         this.y = y;
+    }
+
+    public static Point of(double x, double y) {
+        return new Point(x, y);
     }
 
     public double getX() {
