dcc35ac48246c1ff1a3fcf9bb471dcb1ceef98a2
mk <alabamamidomama@gmail.com>
2022-12-05 15:37:10 +0100

    add: methods to Point class

--- src/entities/Point.java
+++ src/entities/Point.java
@@ -1,22 +1,41 @@
 package entities;
 
 public class Point {
-    private final float x;
-    private final float y;
+    private final double x;
+    private final double y;
     private final static Point START = new Point(0, 0);
     private final static Point START_FROM_X1 = new Point(1, 0);
 
-    public Point(float x, float y) {
+    public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
-    public float getX() {
+    public double getX() {
         return x;
     }
 
-    public float getY() {
+    public double getY() {
         return y;
+    }
+
+    public Point move(double vx, double vy) {
+        return new Point(this.getX() + vx, this.getX() + vy);
+    }
+
+    public Point symmetryOx() {
+        return new Point(this.getX(), -this.getY());
+    }
+
+    public Point symmetryOy() {
+        return new Point(-this.getX(), this.getY());
+    }
+
+    public Point rotate(int degree) {
+        return new Point(
+                this.getX()*Math.cos(degree) - this.getY()*Math.sin(degree),
+                this.getY()*Math.cos(degree) - this.getX()*Math.sin(degree)
+        );
     }
 
     @Override
